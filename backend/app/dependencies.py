20dac607871c6263c6dd62a79f0997f3bd173505
dprasun01 <prasun.dhungana01@bison.howard.edu>
2025-02-24 16:51:53 -0500

    update dependencies with get_current_user

--- backend/app/dependencies.py
+++ backend/app/dependencies.py
@@ -11,8 +11,24 @@ oauth2_scheme = OAuth2PasswordBearer(tokenUrl='/token')
 
 def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)) -> models.User:
   
+  # Defining a credentials exception
   credentials_exception = HTTPException(
     status_code=status.HTTP_401_UNAUTHORIZED,
     detail="Could not validate credentials",
     headers={"WWW-Authenticate" : "Bearer"}
   )
+
+  try:
+    # Decoding the JWT token using the SECRET KEY and HS256 algorithm
+    payload = jwt.decode(token, settings.SECRET_KEY, algorithms=["HS256"])
+    username: str = payload.get("sub")
+    if username is None:
+      raise credentials_exception
+  
+  except JWTError:
+    raise credentials_exception
+  
+  user = crud.get_user_by_username(db, username=username)
+  if user is None:
+    raise credentials_exception
+  return user
